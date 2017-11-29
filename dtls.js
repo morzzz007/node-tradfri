c874c4a057e2099b45f545a45ee3aa17926c7f5b
Márton Papp <morzzz007@gmail.com>
2017-11-29 19:32:08 +0100

    fix(dtls): add meaningful error messages

--- dtls.js
+++ dtls.js
@@ -2,6 +2,17 @@ const args = process.argv.slice(2);
 
 const start = async () => {
   const [hubIpAddress, securityId, coapClientPath] = args;
+
+  if (!hubIpAddress) {
+    console.log('Please provide the ip address of the Tradfri hub as the first argument!')
+    return;
+  }
+
+  if (!securityId) {
+    console.log('Please provide the security code as the second argument!')
+    return;
+  }
+
   const tradfri = require('./index').create({
     coapClientPath: coapClientPath || './lib/coap-client', // use embedded coap-client
     securityId,
